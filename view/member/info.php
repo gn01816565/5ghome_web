1abe28e0b699401dfab7ce1e9a6772faa216a2d6
kai <gn01816565@gmail.com>
2017-04-06 17:58:11 +0800

    增加會員中心的menu資料頁

--- view/member/info.php
+++ view/member/info.php
@@ -2,19 +2,19 @@
 	 <div class="container">
 			<ol class="breadcrumb">
 		    <li><a href="index.php">首頁</a></li>
-		    <li class="active">訂購說明</li>
+		    <li class="active">會員首頁</li>
 		  </ol>
       <div class="col-md-9 product-model-sec">
        <div class="jumbotron">
         <div class="container">
         <!-- 內容 -->
-        訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明訂購說明
+        會員首頁內容
         <!-- 內容 -->
       </div>
     </div>
       </div>
 			<?php
-        include("include_aboutLeftMenu.php");
+        include("view/include/include_member_menu.php");
       ?>
 	   </div>
 		</div>
